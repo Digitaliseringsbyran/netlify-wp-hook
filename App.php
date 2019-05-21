cc86188d1142524334d56fc42873151d0ae19802
David Öhlin <hello@davidohl.in>
2019-05-21 16:50:10 +0200

    folder structure

---
+++ App.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace NetlifyWpHook;
+
+use GuzzleHttp\Client;
+
+class App {
+	protected $BUILD_HOOK_URL;
+
+	public function __construct()
+	{
+		$this->BUILD_HOOK_URL = get_option('netlify_build_hook_url');
+
+		add_action('save_post',array($this, 'send_webhook'), 10, 3);
+		add_action('admin_menu', array($this, 'create_options_page') );
+	}
+
+	/**
+ 	* Send a request to Netlify to build the site on update or save
+ 	* @return void
+ 	*/
+ 	function send_webhook( $post_id, $post, $update ) {
+ 			// Return if build hook isnt set
+ 			if (!is_string($this->BUILD_HOOK_URL) && strlen($this->BUILD_HOOK_URL) === 0) { return; }
+
+		    // Don't fire on blank posts
+			if (isset($post->post_status) && $post->post_status == 'published') { return; }
+
+			// Only run in production
+			// if (defined('WP_ENV') && WP_ENV !== 'production') { return; }
+
+			$client = new \GuzzleHttp\Client();
+			$response = $client->post($this->BUILD_HOOK_URL);
+		}
+
+	/**
+	 * Creates an option page and adds it to the settings menu.
+	 * @return void
+	 */
+	public function create_options_page() 
+	{
+		$title = 'Netlify';
+		$slug = 'netlify';
+		$sectionId = 'Netlify';
+		
+		// Add options page
+		add_options_page(
+			$title, 
+			$title, 
+			'manage_options', 
+			$slug, 
+			array($this, 'render_options_page')
+		);
+
+		// Add settings section to the created options page
+		add_settings_section(
+			$sectionId,   
+			'Netlify',
+			function() {
+				echo "<p>Add a build hook below or <a href='https://www.netlify.com/docs/webhooks/#incoming-webhooks'>read about configuring one</a>.</p>";
+			},
+			$slug
+	    );
+
+		// Add the settings field
+		add_settings_field( 
+			'netlify_build_hook_url',
+			'Build Hook URL',
+			array($this, 'addSettingsFieldCallback'),
+			$slug,
+			$sectionId,
+			array( 'netlify_build_hook_url' )
+		);
+
+		// Register the created fields
+		register_setting( $slug, 'netlify_build_hook_url' );
+	}
+
+	/**
+	 * Callback to add the settings field
+	 * @return void
+	 */
+	public function addSettingsFieldCallback( $args )
+	{
+		echo '<input type="text" placeholder="https://api.netlif..." id="' . $args[0] . '" name="' . $args[0] . '" value="' . get_option($args[0]) . '"/>';
+	}
+
+	/**
+	 * Render the options page
+	 * @return void
+	 */
+	public function render_options_page()
+	{
+		return include 'src/views/form.php';
+	}
+}
--- netlify-wp-hook.php
+++ netlify-wp-hook.php
@@ -10,99 +10,26 @@
  * Text Domain: netlify-wp-hook
  */
 
-require 'vendor/autoload.php';
+define('NWH_PLUGIN_NAME', 'Netlify WP Hook');
+define('NWH_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('NWH_PLUGIN_URL', plugins_url('', __FILE__));
 
-use GuzzleHttp\Client;
-
-class NetlifyWpHook {
-	protected $BUILD_HOOK_URL;
-
-	public function __construct()
-	{
-		$this->BUILD_HOOK_URL = get_option('build_hook_url');
-
-		add_action('save_post',array($this, 'send_webhook'), 10, 3);
-		add_action('admin_menu', array($this, 'create_options_page') );
-	}
-
-	/**
- 	* Send a request to Netlify to build the site on update or save
- 	* @return void
- 	*/
- 	function send_webhook( $post_id, $post, $update ) {
- 			// Return if build hook isnt set
- 			if (!is_string($this->BUILD_HOOK_URL) && strlen($this->BUILD_HOOK_URL) === 0) { return; }
-
-		    // Don't fire on blank posts
-			if (isset($post->post_status) && $post->post_status == 'published') { return; }
-
-			// Only run in production
-			if (defined('WP_ENV') && WP_ENV !== 'production') { return; }
-
-			$client = new \GuzzleHttp\Client();
-			$response = $client->post($this->BUILD_HOOK_URL);
-		}
-
-	/**
-	 * Creates an option page and adds it to the settings menu.
-	 * @return void
-	 */
-	public function create_options_page() 
-	{
-		$title = 'Netlify';
-		$slug = 'netlify';
-		$sectionId = 'Netlify';
-		
-		// Add options page
-		add_options_page(
-			$title, 
-			$title, 
-			'manage_options', 
-			$slug, 
-			array($this, 'render_options_page')
-		);
-
-		// Add settings section to the created options page
-		add_settings_section(
-			$sectionId,   
-			'Netlify',
-			function() {
-				echo "<p>Add a build hook below or <a href='https://www.netlify.com/docs/webhooks/#incoming-webhooks'>read about configuring one</a>.</p>";
-			},
-			$slug
-	    );
-
-		// Add the settings field
-		add_settings_field( 
-			'build_hook_url',
-			'Build Hook URL',
-			array($this, 'addSettingsFieldCallback'),
-			$slug,
-			$sectionId,
-			array( 'build_hook_url' )
-		);
-
-		// Register the created fields
-		register_setting( $slug, 'build_hook_url' );
-	}
-
-	/**
-	 * Callback to add the settings field
-	 * @return void
-	 */
-	public function addSettingsFieldCallback( $args )
-	{
-		echo '<input type="text" placeholder="https://api.netlif..." id="' . $args[0] . '" name="' . $args[0] . '" value="' . get_option($args[0]) . '"/>';
-	}
-
-	/**
-	 * Render the options page
-	 * @return void
-	 */
-	public function render_options_page()
-	{
-		return include 'views/form.php';
-	}
+if (file_exists(NWH_PLUGIN_PATH . 'vendor/autoload.php')) {
+	require_once NWH_PLUGIN_PATH . 'vendor/autoload.php';
 }
 
-new NetlifyWpHook();
+if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
+	require_once dirname(ABSPATH) . '/vendor/autoload.php';
+}
+
+require_once NWH_PLUGIN_PATH . 'src/Vendor/Psr4ClassLoader.php';
+
+$loader = new NetlifyWpHook\Vendor\Psr4ClassLoader();
+$loader->addPrefix('NetlifyWpHook', NWH_PLUGIN_PATH);
+$loader->addPrefix('NetlifyWpHook', NWH_PLUGIN_PATH . 'src/php/');
+$loader->register();
+
+// Initialize plugin
+add_action('plugins_loaded', function () {
+	new NetlifyWpHook\App();
+}, 20);
